edfd9b1e9134052d5942beb4222dc8880f3c0271
Lars Nysom <LANY@ucn.dk>
2021-09-20 12:44:36 +0200

    Add project files.

---
+++ Search/Search.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+namespace Search
+{
+    public class Search
+    {
+        private static List<Person> persons = new List<Person>()
+        {
+            new Person{Id = 1, Name = "Hans"},
+            new Person{Id = 2, Name = "Knud"}
+        };
+
+
+        public Person Find(int? id = null, string name = null)
+        {
+            Person result = null;
+
+            if (name != null)
+            {
+                // Search database by Name
+                result = persons.Find(p => p.Name == name);
+            }
+            else if (id != null)
+            {
+                // Search database by ID
+                result = persons.Find(p => p.Id == id);
+            }
+            return result;
+        }
+
+        //public Person Find(string name)
+        //{
+        //    // Search database by Name
+        //    return persons.Find(p => p.Name == name);
+        //}
+
+
+        //private Person FindById(int id)
+        //{
+        //    // Search database by ID
+        //    return persons.Find(p => p.Id == id);
+        //}
+
+        //private Person FindByName(string name)
+        //{
+        //    // Search database by Name
+        //    return persons.Find(p => p.Name == name);
+        //}
+    }
+}
---
+++ SearchTests/SearchTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Search;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Search.Tests
+{
+    [TestClass()]
+    public class SearchTests
+    {
+        [TestMethod()]
+        public void FindByIdTest()
+        {
+            Search s = new Search();
+
+            Person result = s.Find(id: 1);
+
+            Assert.AreEqual("Hans", result.Name);
+
+        }
+
+        [TestMethod()]
+        public void FindByNameTest()
+        {
+            Search s = new Search();
+
+            Person result = s.Find(name: "Hans");
+
+            Assert.AreEqual(1, result.Id);
+        }
+    }
+}
